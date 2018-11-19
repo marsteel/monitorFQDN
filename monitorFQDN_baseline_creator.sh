c74c283b3d9581b63dd7e70ec695db842472d562
MA Gang <magang@gmail.com>
2018-11-19 16:38:36 +0800

    Update monitorFQDN_baseline_creator.sh

--- monitorFQDN_baseline_creator.sh
+++ monitorFQDN_baseline_creator.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
+#Specify the host part of the FQDN if they are in same domain. Or specify whole FQDN and change line 9 accordingly.
 declare -a Target_Hostname_array=(example_host1  example_host2)
 
 for Target_Hostname in "${Target_Hostname_array[@]}"
 do
-
+        #Specify the domain part of the FQDN
         this_hostname=${Target_Hostname}.example-domain.com
-                
-                dig +answer ${this_hostname} +short | tail -n4 > ${this_hostname}.txt
+        # tail -n4 returns first 4 lines of dig ouput, it should be enough to retrieve all A record. 
+        # Increase it if there are more A records for the FQDN
+        dig +answer ${this_hostname} +short | tail -n4 > ${this_hostname}.txt
         echo "${this_hostname} baseline file created!"
 done
